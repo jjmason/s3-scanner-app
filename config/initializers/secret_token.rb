8f615c6ac529afcb9393b15924d195055e379fbb
Jon <jonathan.j.mason@gmail.com>
2013-03-28 20:07:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3ScannerApp::Application.config.secret_token = '4967c6e361eb6971690c77a6da9ced9f3ce2240c021c28fca7cd31eb1bc4b9bf49156771767fdc714d82354b44a86f8d163f59270f5f175d26431d5df59bc2fc'
